8059d49af2192efc6c57207d3b2e8b8bd5d07e9d
funky1119 <funky1119@naver.com>
2024-04-09 16:31:29 +0900

    API 호출 및 동적 메타 데이터 처리 방식 추가

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,12 +1,21 @@
 import { Suspense } from "react";
-import { API_URL } from "../../../(home)/page";
-import MovieInfo from "../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../components/movie-info";
 import MovieVideos from "../../../components/movie-videos";
-// nomad-movies.nomadcoders.workers.dev
 
-export default async function Movies({ params }) {
-  const { id } = params;
+interface IParams {
+  params: {
+    id: string;
+  };
+}
 
+export async function generateMetadata({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default async function MovieDetailPage({ params: { id } }) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -2,11 +2,12 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import steyls from "../../styles/navigation.module.css";
 
 export default function Navigation() {
   const path = usePathname();
   return (
-    <nav>
+    <nav className={steyls.nav}>
       <ul>
         <li>
           <Link href="/">Home</Link>
